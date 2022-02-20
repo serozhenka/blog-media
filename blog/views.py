dcb956376a03d9ee1072fe24ae0a6ff80109b4a5
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-02-20 13:37:11 +0200

    template created

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': "Corey MS",
+        'title': 'blog1',
+        'content': 'blog1_content',
+        'date': '12-18-2000',
+    },
+    {
+        'author': "Jane Doe",
+        'title': 'blog2',
+        'content': 'blog2_content',
+        'date': '11-12-2001',
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+        'title': 'blogPage',
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
 
