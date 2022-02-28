fc29edfd8ce7f64f9a905c393a84ee6a554a3100
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-02-28 21:46:24 +0200

    pagination added

--- blog/urls.py
+++ blog/urls.py
@@ -5,14 +5,16 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 from . import views
 
 urlpatterns = [
     path('about/', views.about, name='blog-about'),
     path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('new', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
